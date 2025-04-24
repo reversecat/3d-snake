2b499e8b23614ade03f174268fe3760e140a6d72
Daniel Sobkowicz <daniel.a.sobkowicz@gmail.com>
2025-04-24 10:37:38 -0400

    Finished 2d snake base

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 const BLOCK_SIZE = 25;
+const MOVES_PER_SECOND = 8;
 
 enum Direction {
 	Up,
@@ -28,17 +29,28 @@ interface Rect {
 }
 
 function collisionBoundingBox(r1: Rect, r2: Rect): boolean {
-	if (r1.x + r1.w >= r2.x && r1.x <= r2.x + r2.w && r1.y + r1.h >= r2.y && r1.y <= r2.y + r2.h) {
+	if (r1.x + r1.w > r2.x && r1.x < r2.x + r2.w && r1.y + r1.h > r2.y && r1.y < r2.y + r2.h) {
 		return true;
 	}
 	return false;
 }
 
+class SnakeBody {
+	pos: Point;
+	active: number; // if 0 is active, otherwise its a postive number that decrements each tick
+
+	constructor(x: number, y: number, active: number = 0) {
+		this.pos = new Point(x, y);
+		this.active = active;
+	}
+}
+
 class Snake {
 	direction: Direction;
-	body: Array<Point>;
+	body: Array<SnakeBody>;
 	size: number;
 	speed: number;
+	alive: boolean = true;
 
 	constructor(x: number, y: number) {
 		this.direction = Direction.None;
@@ -46,26 +58,46 @@ class Snake {
 		this.size = BLOCK_SIZE;
 		this.speed = BLOCK_SIZE;
 
-		this.body.push(new Point(x, y));
+		this.body.push(new SnakeBody(x, y));
 	}
 
-	checkCollisions = (): boolean => {
-		const head = this.body[0];
+	getHead = (): Point => {
+		return this.body[0].pos;
+	};
+
+	getTail = (): Point => {
+		return this.body[this.body.length - 1].pos;
+	};
+
+	checkCollisions = (): void => {
+		// Self collision:
 		// Don't need to check self collisions if snake length < 4
 		for (let i = 3; i < this.body.length; i++) {
-			const rect1 = { x: head.x, y: head.y, w: this.size, h: this.size };
+			if (this.body[i].active !== 0) continue; // since new pieces stay at head until snake moves past
+			const rect1 = { x: this.getHead().x, y: this.getHead().y, w: this.size, h: this.size };
 			const rect2 = {
-				x: this.body[i].x,
-				y: this.body[i].y,
+				x: this.body[i].pos.x,
+				y: this.body[i].pos.y,
 				w: this.size,
 				h: this.size,
 			};
-			if (collisionBoundingBox(rect1, rect2)) return true;
+			if (collisionBoundingBox(rect1, rect2)) this.die();
 		}
-		return false;
+
+		// Off screen collision:
+		if (
+			this.getHead().x < 0 ||
+			this.getHead().x >= canvas.width ||
+			this.getHead().y < 0 ||
+			this.getHead().y >= canvas.height
+		) {
+			this.die();
+		}
 	};
 
-	snakeDeath = (): void => {};
+	die = (): void => {
+		this.alive = false;
+	};
 
 	setDirection = (direction: Direction): void => {
 		if (this.body.length === 1) this.direction = direction;
@@ -83,36 +115,42 @@ class Snake {
 		}
 	};
 	move = (): void => {
+		// for death animation:
+		if (!this.alive) {
+			for (let i = 0; i < this.body.length; i++) {
+				if (this.body[i].active != -1) {
+					this.body[i].active = -1;
+					return;
+				}
+			}
+			return;
+		}
+
 		// Loop from tail to head, moving each piece to the one ahead of it
 		for (let i = this.body.length - 1; i > 0; i--) {
-			this.body[i] = this.body[i - 1];
+			if (this.body[i].active === 0) {
+				this.body[i].pos.x = this.body[i - 1].pos.x;
+				this.body[i].pos.y = this.body[i - 1].pos.y;
+			} else {
+				this.body[i].active--;
+			}
 		}
 		// Move snake head
 		switch (this.direction) {
 			case Direction.Up:
-				this.body[0].y -= this.speed;
+				this.getHead().y -= this.speed;
 				break;
 			case Direction.Right:
-				this.body[0].x += this.speed;
+				this.getHead().x += this.speed;
 				break;
 			case Direction.Down:
-				this.body[0].y += this.speed;
+				this.getHead().y += this.speed;
 				break;
 			case Direction.Left:
-				this.body[0].x -= this.speed;
+				this.getHead().x -= this.speed;
 				break;
 		}
 	};
-}
-
-class food {
-	pos: Point;
-	color: string;
-
-	constructor(x: number, y: number) {
-		this.pos = new Point(x, y);
-		this.color = "red";
-	}
 }
 
 type foodPositions = {
@@ -130,10 +168,10 @@ function createFood() {
 		}
 	}
 	// Remove overlapping points with Snake
-	const snakeBody: Array<Point> = player.body;
+	const snakeBody: Array<SnakeBody> = player.body;
 	snakeBody.forEach((cell) => {
-		if (cell.toStr() in possible_pts) {
-			delete possible_pts[cell.toStr()];
+		if (cell.pos.toStr() in possible_pts) {
+			delete possible_pts[cell.pos.toStr()];
 		}
 	});
 
@@ -145,6 +183,11 @@ function createFood() {
 	});
 
 	let possible_pts_arr: string[] = Object.keys(possible_pts);
+
+	// TODO: Handle no more space for food
+
+	if (possible_pts_arr.length <= 0) throw new Error("No possible points for food");
+
 	let random_pos: string = possible_pts_arr[Math.floor(Math.random() * possible_pts_arr.length)];
 	let new_food = new Point(
 		parseInt(random_pos.split(",")[0]),
@@ -183,28 +226,52 @@ window.addEventListener(
 function gameDraw() {
 	ctx!.fillStyle = "black";
 	ctx?.fillRect(0, 0, canvas.width, canvas.height);
-	// draw snake
-	ctx!.fillStyle = "green";
-	const snakeBody: Array<Point> = player.body;
-	snakeBody.forEach((cell) => {
-		ctx?.fillRect(cell.x, cell.y, player.size, player.size);
-	});
+
 	// draw food
 	ctx!.fillStyle = "red";
 	foodArr.forEach((cell) => {
 		ctx?.fillRect(cell.x, cell.y, BLOCK_SIZE, BLOCK_SIZE);
 	});
+
+	// draw snake
+	ctx!.fillStyle = "green";
+	const snakeBody: Array<SnakeBody> = player.body;
+	snakeBody.forEach((cell) => {
+		if (cell.active === -1) ctx!.fillStyle = "gray";
+		ctx?.fillRect(cell.pos.x, cell.pos.y, player.size, player.size);
+	});
 }
 
 // Game loop
-function gameLoop() {
+function gameTick() {
 	gameDraw();
+
+	// check food collisions
+	let removeIdx = -1;
+	for (let i = 0; i < foodArr.length; i++) {
+		const curFood = foodArr[i];
+		const snakeHead = player.getHead();
+		if (
+			collisionBoundingBox(
+				{ x: snakeHead.x, y: snakeHead.y, w: player.size, h: player.size },
+				{ x: curFood.x, y: curFood.y, w: BLOCK_SIZE, h: BLOCK_SIZE }
+			)
+		) {
+			player.body.push(new SnakeBody(curFood.x, curFood.y, player.body.length));
+			removeIdx = i;
+			break;
+		}
+	}
+	if (removeIdx !== -1) {
+		foodArr.splice(removeIdx, 1);
+		createFood();
+	}
+
 	player.checkCollisions();
 	player.move();
 }
 
-createFood();
-setInterval(gameLoop, 150);
-
-// TODO: Add snake eat food
-// Snake death on self / out of screen
+for (let i = 0; i < 5; i++) {
+	createFood();
+}
+setInterval(gameTick, Math.floor(1000 / MOVES_PER_SECOND));
